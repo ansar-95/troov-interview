0c2a034731a219e01811e0cc139d0206911c49c4
Ansar <moha.ans@hotmail.com>
2023-02-14 22:29:32 +0100

    add readme

--- my-project/.eslintrc.js
+++ my-project/.eslintrc.js
@@ -11,5 +11,57 @@ module.exports = {
   extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    indent: "off",
+    "linebreak-style": ["error", "unix"],
+    quotes: [
+      "error",
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    semi: ["error", "never"],
+    "no-console": "error",
+    "no-implicit-globals": "error",
+    "no-warning-comments": ["error", { terms: ["fixme", "todo"] }],
+    "newline-before-return": "error",
+    curly: "error",
+    "padded-blocks": ["error", "never"],
+    "space-before-blocks": "error",
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: [
+          "break",
+          "case",
+          "cjs-export",
+          "class",
+          "continue",
+          "do",
+          "if",
+          "switch",
+          "try",
+          "while",
+          "return",
+        ],
+      },
+      {
+        blankLine: "always",
+        prev: [
+          "break",
+          "case",
+          "cjs-export",
+          "class",
+          "continue",
+          "do",
+          "if",
+          "switch",
+          "try",
+          "while",
+          "return",
+        ],
+        next: "*",
+      },
+    ]
+  },
 }
